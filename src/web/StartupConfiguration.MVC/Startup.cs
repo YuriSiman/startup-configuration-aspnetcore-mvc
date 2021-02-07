fc55e35a13640720e91263656915e960a59a133b
Yuri Siman <yurisimannogueira@gmail.com>
2021-02-07 00:15:18 -0300

    Definindo variáveis de ambientes no construtor da classe Startup

--- src/web/StartupConfiguration.MVC/Startup.cs
+++ src/web/StartupConfiguration.MVC/Startup.cs
@@ -2,18 +2,25 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using StartupConfiguration.MVC.Configuration;
 
 namespace StartupConfiguration.MVC
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
         public IConfiguration Configuration { get; }
+
+        public Startup(IHostEnvironment hostEnvironment)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(hostEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{hostEnvironment.EnvironmentName}.json", true, true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
 
         public void ConfigureServices(IServiceCollection services)
         {
